80b8785a06de02d19645ecb6dd334f9227d1ecc3
Sofiaevx <villanueva19.sofia@gmail.com>
2022-02-09 22:53:27 +0800

    Done creating jsonfile , pdffile, and temporary design. Putting my fake information in json and trying to transfer it in pdf.

---
+++ PDFRESUMECREATOR/ResumeCreator.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace PDFRESUMECREATOR
+{
+    partial class ResumeCreator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonConvert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonConvert
+            // 
+            this.buttonConvert.Location = new System.Drawing.Point(230, 120);
+            this.buttonConvert.Name = "buttonConvert";
+            this.buttonConvert.Size = new System.Drawing.Size(75, 23);
+            this.buttonConvert.TabIndex = 0;
+            this.buttonConvert.Text = "Convert";
+            this.buttonConvert.UseVisualStyleBackColor = true;
+            this.buttonConvert.Click += new System.EventHandler(this.buttonConvert_Click);
+            // 
+            // ResumeCreator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(338, 155);
+            this.Controls.Add(this.buttonConvert);
+            this.Name = "ResumeCreator";
+            this.Text = "PDF RESUME CREATOR";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonConvert;
+    }
+}
+
---
+++ PDFRESUMECREATOR/ResumeCreator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+using Newtonsoft.Json;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace PDFRESUMECREATOR
+{
+    public partial class ResumeCreator : Form
+    {
+        string jsonpath = "SofiaVillanueva.json";
+        public ResumeCreator()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonConvert_Click(object sender, EventArgs e)
+        {
+            string openjson = File.ReadAllText(jsonpath);
+            Information_json output = JsonConvert.DeserializeObject<Information_json>(openjson);
+
+            Document jsontopdf = new Document();
+            PdfWriter.GetInstance(jsontopdf, new FileStream("VILLANUEVA_SOFIARUTH.pdf", FileMode.Create));
+            jsontopdf.Open();
+            Paragraph myimage = new Paragraph(output.MYIMAGE);
+            Paragraph f_name = new Paragraph(output.FULLNAME);
+            Paragraph age = new Paragraph(output.AGE);
+
+
+            PdfPTable table = new PdfPTable(3);
+            table.HorizontalAlignment = 1;
+            table.WidthPercentage = 100f;
+
+            table.AddCell(myimage);
+            table.AddCell(f_name);
+            table.AddCell(age);
+
+            jsontopdf.Add(table);
+
+            
+            jsontopdf.Close();
+            MessageBox.Show("Pdf Create Successfully!!!");
+
+
+        }
+
+        public class Information_json
+        {
+            public string MYIMAGE { get; set; }
+            public string FULLNAME{ get; set; }
+            public string AGE { get; set; }
+        }
+    }
+}
