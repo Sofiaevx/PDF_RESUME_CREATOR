80f2d9f36449da71cfb0aca393fdd8a324a0a588
Sofiaevx <villanueva19.sofia@gmail.com>
2022-02-10 00:04:03 +0800

    done creating a header for my resume

--- PDFRESUMECREATOR/ResumeCreator.cs
+++ PDFRESUMECREATOR/ResumeCreator.cs
@@ -17,6 +17,7 @@ namespace PDFRESUMECREATOR
 {
     public partial class ResumeCreator : Form
     {
+        //this json path leads to json file, located at debug folder 
         string jsonpath = "SofiaVillanueva.json";
         public ResumeCreator()
         {
@@ -26,23 +27,49 @@ namespace PDFRESUMECREATOR
         private void buttonConvert_Click(object sender, EventArgs e)
         {
             string openjson = File.ReadAllText(jsonpath);
-            Information_json output = JsonConvert.DeserializeObject<Information_json>(openjson);
 
+            //Organizing json context
+            Information_json output = JsonConvert.DeserializeObject<Information_json>(openjson);
+            //creating pdf file located at bin-> Debug folder
             Document jsontopdf = new Document();
             PdfWriter.GetInstance(jsontopdf, new FileStream("VILLANUEVA_SOFIARUTH.pdf", FileMode.Create));
             jsontopdf.Open();
-            Paragraph myimage = new Paragraph(output.MYIMAGE);
+
+            //for json file value
+            iTextSharp.text.Image myimage = iTextSharp.text.Image.GetInstance(output.MYIMAGE);
             Paragraph f_name = new Paragraph(output.FULLNAME);
             Paragraph age = new Paragraph(output.AGE);
 
+            myimage.ScaleAbsolute(100, 100);
+           
+          
+            f_name.Font.Size = 15;
+            f_name.Font.Color = BaseColor.WHITE;
+            age.Font.Size = 15;
+            age.Font.Color = BaseColor.WHITE;
 
+            //table
             PdfPTable table = new PdfPTable(3);
             table.HorizontalAlignment = 1;
             table.WidthPercentage = 100f;
 
-            table.AddCell(myimage);
-            table.AddCell(f_name);
-            table.AddCell(age);
+            PdfPCell my_img = new PdfPCell(myimage, false);
+            PdfPCell my_fullname = new PdfPCell(new Phrase(f_name));
+            PdfPCell my_age = new PdfPCell(new Phrase(age));
+
+            my_img.BackgroundColor = BaseColor.BLACK;
+            my_fullname.HorizontalAlignment = Element.ALIGN_CENTER;
+            my_fullname.VerticalAlignment = Element.ALIGN_MIDDLE;
+            my_fullname.BackgroundColor = BaseColor.BLACK;
+
+            my_age.HorizontalAlignment = Element.ALIGN_RIGHT;
+            my_age.VerticalAlignment = Element.ALIGN_MIDDLE;
+            my_age.BackgroundColor = BaseColor.BLACK;
+            //inserting data to pdf
+            table.AddCell(my_img);
+            table.AddCell(my_fullname);
+            table.AddCell(my_age);
+            
 
             jsontopdf.Add(table);
 
